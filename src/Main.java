03ed8e18875db3d7a71d745d2fd9c2a6c23b97a4
colmlg <colm.legear@gmail.com>
2018-10-31 16:28:45 +0000

    Base Setup for main menu

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import UI.MainMenu;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainMenu menu = new MainMenu();
+    }
+}
