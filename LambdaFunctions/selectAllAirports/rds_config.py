f4e5449f254d4b67940f1419e7eca2f704a412e3
William <willoleary6@gmail.com>
2018-11-06 20:55:35 +0000

    just leaving the zip files for the lambda functions

--- LambdaFunctions/selectAllAirports/rds_config.py
+++
@@ -1,5 +0,0 @@
-#config file containing credentials for rds mysql instance
-db_username = "willoleary6"
-db_password = "sfSDHFDJFHEj23120-94cdpo"
-db_name = "canoe"
-rds_host = "canoe.crait7r8hupj.eu-west-1.rds.amazonaws.com"
